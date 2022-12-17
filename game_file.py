94c484fc7c9a6f45edfb22c4f9af09523d9399fa
Victoria Brunner <astrobrunner@gmail.com>
2022-12-17 18:01:50 -0500

    Add in Vacations story

--- game_file.py
+++ game_file.py
@@ -1,1 +1,15 @@
-#File for Mad Libs game
+from story_directory.story_list import *
+from story_directory.stories import *
+from story_directory.userq import *
+from random import choice
+
+choose_story_name = choice(STORYNAMES)
+user_qs = STORYQUESTIONS[choose_story_name]
+
+answerlist = []
+for question in user_qs:
+    answerlist.append(input(question))
+
+choose_story = storiesdict(choose_story_name, answerlist)
+
+print(choose_story)
