226c127b3e0a8a7645ba94a34942fd7bdbd97b91
Victoria Brunner <astrobrunner@gmail.com>
2022-12-18 12:58:55 -0500

    removed test directories and files to avoid future user confusion

--- test_story_directory/test_stories.py
+++
@@ -1,6 +0,0 @@
-def storiesdict(storyname, answerlist):
-    storydictionary = {
-        "test1" : "This is {}.".format(answerlist[0]),
-        "test2" : "Hello, and welcome to {}.".format(answerlist[0]),
-    }
-    return storydictionary[storyname]
