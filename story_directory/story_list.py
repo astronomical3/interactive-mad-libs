51a72105f3a00a1c52fbeda733d653d1c99a1eec
Victoria Brunner <astrobrunner@gmail.com>
2022-12-19 21:15:36 -0500

    Added 11 full stories to library

--- story_directory/story_list.py
+++ story_directory/story_list.py
@@ -1,3 +1,14 @@
 STORYNAMES = [
     "Vacations",
+    "Principal for a Day",
+    "Santa Claus Coming",
+    "Valentines",
+    "Lunchroom",
+    "Halloween Monsters",
+    "Special Mother",
+    "Twas Night Before",
+    "Birthday",
+    "All About Me",
+    "Bakery",
+    "Mountain Hike",
 ]
